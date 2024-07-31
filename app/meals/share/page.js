1749bb52934d77ed9bd14d98fbe9d7709b5fa6d1
Federico.Murru <djfedemurru@icloud.com>
2024-07-31 09:40:19 +0100

    correction

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -19,7 +19,7 @@ export default function ShareMealPage() {
 				<p>Or any other meal you feel needs sharing!</p>
 			</header>
 			<main className={classes.main}>
-				<form className={classes.form} action={shareMeal}>
+				<form className={classes.form} action={formAction}>
 					<div className={classes.row}>
 						<p>
 							<label htmlFor="name">Your name</label>
@@ -48,6 +48,7 @@ export default function ShareMealPage() {
 						></textarea>
 					</p>
 					<ImagePicker label="your image" name="image" />
+					{state.message && <p>{state.message}</p>}
 					<p className={classes.actions}>
 						<MealsFormSubmit />
 					</p>
